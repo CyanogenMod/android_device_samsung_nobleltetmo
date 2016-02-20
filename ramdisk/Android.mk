dcd1e3eb3a87d43d4ede11fa4e0e43eb22c36151
Christopher N. Hesse <raymanfx@gmail.com>
2016-02-20 02:42:05 +0100

    nobleltetmo: Fix baseband script location

--- ramdisk/Android.mk
+++ ramdisk/Android.mk
@@ -6,7 +6,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE		:= init.baseband.rc
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= BOOT
-LOCAL_SRC_FILES		:= etc/init.baseband.rc
+LOCAL_SRC_FILES		:= init.baseband.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
